2219d3874a7e779702ee50b7c484a28c4d80a230
unknown <Alex@Alex-HP.(none)>
2011-11-25 19:26:15 -0600

    tarea.java

---
+++ Tarea.java
@@ -0,0 +1,27 @@
+public class Tarea{
+	
+	private int id;
+	private int procs;
+	
+	public Tarea(int id, int procs){
+		this.id = id;
+		this.procs = procs;
+	}
+		
+	public int getId(){
+		return this.id;
+	}
+	
+	public void setId(int id){
+		this.id = id;
+	}
+		
+	public int getProcs(){
+		return this.procs;
+	}
+	
+	public void setId(int procs){
+		this.procs = procs;
+	}
+	
+}
